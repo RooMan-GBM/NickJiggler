aef1c4b9941e335df95692501d45e7a14523cbb8
Ray <xxroomanxx@gmail.com>
2019-03-01 02:26:10 +0000

    Add files via upload

---
+++ img.sh
@@ -0,0 +1,11 @@
+#convert PIMG.jpeg -thumbnail 134x92 THMB.jpeg
+
+for jp in images/*.*; do
+##[ -e "$jp" ] || continue
+ convert "$jp" -thumbnail 640x440 PIMG.jpeg
+ convert PIMG.jpeg -thumbnail 134x92 THMB.jpeg
+ echo $(basename "${jp%%.*}")
+ ./nickjiggler $(basename "${jp%%.*}" )
+ mv *.jig custom/
+ rm PIMG.jpeg THMB.jpeg
+done
